e18774f324a7bb9290f852aa83295d42a8823011
Romarico Vargas <s3884395@student.rmit.edu.au>
2024-11-11 19:07:11 +1100

    Started problem 11

---
+++ Set1 0001 to 0050/problem 0011.py
@@ -0,0 +1,13 @@
+'''
+In the 20 x 20 grid below, four numbers along a diagonal line have been marked in red.
+
+The product of these numbers is 26 x 63 x 78 x 14 = 1788696
+
+What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20 x 20 grid?
+
+'''
+
+def problem0011():
+    print("Hello")
+
+problem0011()
