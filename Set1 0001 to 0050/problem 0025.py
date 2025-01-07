37f96c86f92e5a4620acf595d0de676e816054bb
Romarico Vargas <romaricovargas@yahoo.com>
2025-01-07 20:16:15 +1100

    resolved problem 25

--- Set1 0001 to 0050/problem 0025.py
+++ Set1 0001 to 0050/problem 0025.py
@@ -22,25 +22,45 @@ The 12th term, F(12), is the first term to contain three digits.
 
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
 
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+Note:
+
+Python only allow calling a recursive function for 1000 times.
+This can be extended to 10**6 times using the following codes:
+
+    import sys
+    sys.setrecursionlimit(10**6)
+
+However, this is still not enough for my solution. 
+To address this, I placed the recursive call within a loop, 
+ensuring that each recursive call does not exceed 1000 iterations.
+
 '''
 
-import sys
-sys.setrecursionlimit(10**6)
+#import sys
+#sys.setrecursionlimit(10**6)
 
 def problem_0025():
 
     def fibonacci(x, y, i, max):
+        #recursive function that will generate the fibonacci series until max is reached
         j = i + 1
         z = x + y
-
         if z < max:
-            r = fibonacci(y, z, j, max)
+            return fibonacci(y, z, j, max)
         else:
-            r = j
+            return y, z, j
 
-        return r
+    # Initial values
+    p = 200  #number of digits
+    a = 1    #first number
+    b = 1    #second number
+    c = 2    #index of second number
 
-    print(fibonacci(1,1,2,100))
-       
+    for p in range(200, 1001, 200):
+        a, b, c = fibonacci(a,b,c,10**(p-1))            # invoke recursive iterations several times
+        
+    return c     
 
-problem_0025()
+print(problem_0025())
